7d3e33babaff859f5304b48b68ad59bab6e8bb82
viper23 <htonnycarlos@gmail.com>
2022-11-25 15:47:03 +0100

    db config

--- django_course_return/settings.py
+++ django_course_return/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_course_return.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangocourse_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
