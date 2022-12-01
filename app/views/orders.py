7e8277862e3f6bf198ffd35c75db8e030252a2fa
viper23 <htonnycarlos@gmail.com>
2022-12-01 10:02:56 +0100

    add edit view

--- app/views/orders.py
+++ app/views/orders.py
@@ -59,3 +59,36 @@ def store(request):
             form.save()
         messages.success(request, "Order has been saved successfully !")
         return redirect('/orders')
+
+def edit(request, id):
+    assert isinstance(request, HttpRequest)
+    if request.method == "GET":
+        if id == 0:
+            form = OrderForm()
+        else:
+            order = Order.objects.get(pk=id)
+            form = OrderForm(instance=order)
+        return render(
+            request,
+            'app/orders/edit.html',
+            {
+                'form': form
+            }
+        )
+    else:
+        if id == 0:
+            form = OrderForm(request.POST)
+        else:
+            order = Order.objects.get(pk=id)
+            form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Order successfully modified")
+        return redirect('/orders')
+
+def delete(request, id):
+    order = Order.objects.get(pk=id)
+    order.delete()
+    messages.success(request, "Order successfully deleted")
+    return redirect('/orders')
+  
